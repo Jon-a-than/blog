793a3a32812b3247da0a9cd90567a65315492260
Jon-a-than <2140145186@qq.com>
2024-04-27 22:44:22 +0800

    chore: update project config files

--- uno.config.ts
+++ uno.config.ts
@@ -1,23 +1,13 @@
-import {
-  defineConfig,
-  presetAttributify,
-  presetIcons,
-  presetUno,
-  transformerDirectives
-} from 'unocss'
-
-import { colors } from 'unocss/preset-mini'
+import { defineConfig, presetIcons, presetUno, transformerDirectives } from 'unocss'
 
 export default defineConfig({
   content: {
     pipeline: {
-      include: [/\.([jt]sx|mdx?|html)($|\?)/]
+      include: [/\.([jt]sx|mdx?|html|astro)($|\?)/]
     }
   },
   presets: [
-    presetAttributify({ prefix: 'v:' }),
     presetIcons({
-      cdn: 'https://fastly.jsdelivr.net/npm/',
       extraProperties: { display: 'inline-block', 'vertical-align': 'middle' }
     }),
     presetUno()
@@ -26,46 +16,46 @@ export default defineConfig({
   safelist: [],
   shortcuts: {
     'scrollbar-slim':
-      'scrollbar:(w-2 h-2 bg-transparent) scrollbar-corner:bg-transparent scrollbar-thumb:(rounded bg-primary dark:bg-primary-6 h-2 w-2)'
+      'scrollbar:(w-2 h-2 bg-transparent) scrollbar-corner:bg-transparent scrollbar-thumb:(rounded bg-snow-5 dark:bg-night-4 h-2 w-2)'
   },
   theme: {
     colors: {
-      ...colors,
-      get primary(): typeof colors.slate {
-        return this.slate
+      night: {
+        1: '#4C566A',
+        2: '#434C5E',
+        3: '#3B4252',
+        4: '#2E3440',
+        5: '#242933'
       },
-      get primaryDark(): typeof colors.slate {
-        return this.slate
+      snow: {
+        1: '#FFFFFF',
+        2: '#F2F4F8',
+        3: '#ECEFF4',
+        4: '#E5E9F0',
+        5: '#D8DEE9'
+      },
+      frost: {
+        1: '#8FBCBB',
+        2: '#88C0D0',
+        3: '#81A1C1',
+        4: '#5E81AC'
       }
     }
   },
   transformers: [transformerDirectives()],
   variants: [
     (matcher) => {
-      if (!matcher.startsWith('scrollbar:')) {
-        return matcher
-      }
+      if (!matcher.startsWith('scrollbar:')) return matcher
       return {
         matcher: matcher.slice(10),
         selector: (s) => `${s}::-webkit-scrollbar`
       }
     },
     (matcher) => {
-      if (!matcher.startsWith('scrollbar-thumb:')) {
-        return matcher
-      }
+      if (!matcher.startsWith('scrollbar-thumb:')) return matcher
       return {
         matcher: matcher.slice(16),
         selector: (s) => `${s}::-webkit-scrollbar-thumb`
-      }
-    },
-    (matcher) => {
-      if (!matcher.startsWith('scrollbar-corner:')) {
-        return matcher
-      }
-      return {
-        matcher: matcher.slice(17),
-        selector: (s) => `${s}::-webkit-scrollbar-corner`
       }
     }
   ]
