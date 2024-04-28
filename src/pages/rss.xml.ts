52b7efa46f07cbb61b9714d8a454cae8bdb9389e
Jon-a-than <2140145186@qq.com>
2024-04-28 23:37:17 +0800

    feat: init content page

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,12 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
-export async function GET(context) {
+export async function GET(context: Request) {
   const posts = await getCollection('posts')
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: context.site,
+    site: context.url,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
