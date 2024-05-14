1c2453015293ff93ef9cac9919d72018477cb758
Jon-a-than <2140145186@qq.com>
2024-05-14 23:31:44 +0800

    feat: update rss config

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,17 +2,32 @@ import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
 export async function GET(context: Request) {
-  const posts = await getCollection('posts')
+  const posts = await Promise.all([
+    getCollection('posts'),
+    getCollection('blogs'),
+    getCollection('notes')
+  ])
+
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title: 'QingShaner | Blog',
+    description: '一个博客站点',
     site: context.url,
-    items: posts.map((post) => ({
+    items: posts.flat().map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/posts/${post.slug}/`
     })),
-    customData: '<language>en-us</language>'
+    customData: customDataHelper({
+      language: 'zh-CN',
+      lastBuildDate: new Date().toUTCString()
+    })
   })
+}
+
+function customDataHelper(customData: Record<string, string>) {
+  return Object.entries(customData).reduce(
+    (xml, [key, value]) => `${xml}<${key}>${value}</${key}>`,
+    ''
+  )
 }
